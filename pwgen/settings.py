8bd16d8f11615c617a1acb57480c4b2c2d361a04
Aaron Little <aplittle78@gmail.com>
2020-07-01 14:52:11 +0000

    Added base.html to templates folder

--- pwgen/settings.py
+++ pwgen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'c&^=(=r44h(kx1-3x_ad-l2mn#t=t6(8d6y)=h3et8a%w0^hk%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aplittle78.pythonanywhere.com']
 
 
 # Application definition
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
