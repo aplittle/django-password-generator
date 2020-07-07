aa8063a071a1f716569669d002c91a3265568132
aplittle78 <aplittle78@gmail.com>
2020-07-07 10:24:52 -0400

    updated base.html files to correct caps again. Added app for loot tracking.

--- pwgen/urls.py
+++ pwgen/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('generator_home/', include('generator.urls')),
     path('', include('dashboard.urls')),
+    path('loot/', include('loot.urls')),
 ]
