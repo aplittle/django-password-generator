cf6d75458b3b937c4c5c907ff31cb326edd4403c
aplittle78 <aplittle78@gmail.com>
2020-07-07 10:48:01 -0400

    Corrected template path again

--- pwgen/urls.py
+++ pwgen/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('generator_home/', include('generator.urls')),
     path('', include('dashboard.urls')),
-    path('loot/', include('loot.urls')),
+    path('loot_home/', include('loot.urls')),
 ]
